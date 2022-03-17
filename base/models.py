271e885acef8e2ad60646f58c1813545dce73af3
Obi Enechionyia <obi@MacBook-Pro.local>
2022-03-17 20:35:14 +0100

    Create forms.py and update models

--- base/models.py
+++ base/models.py
@@ -5,5 +5,6 @@ from phonenumber_field.modelfields import PhoneNumberField
 class Member(models.Model):
     user = models.ForeignKey(
     User, on_delete=models.CASCADE, null=True, blank=True)
+    first_name = models.CharField(null=True, blank=True, max_length=200)
     phone = PhoneNumberField(null=False, blank=False, unique=True)
     created = models.DateTimeField(auto_now_add=True)
