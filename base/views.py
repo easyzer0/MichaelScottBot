f198bf22888237162a9dce7f7e224c3c7c1034e3
Obi Enechionyia <obi@MacBook-Pro.local>
2022-03-17 21:28:19 +0100

    Updated admin, forms, views, and urls

--- base/views.py
+++ base/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .forms import MemberForm
 
-# Create your views here.
+def index(request):
+    form = MemberForm()
+    if request.method == 'POST':
+        print(request.POST)
+        form = MemberForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {'form':form}
+    return render(request, 'base/index.html', context)
