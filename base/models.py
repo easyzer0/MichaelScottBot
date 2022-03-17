52979a27e51fbcad17d9c48d315d92b76501bbe0
Obi Enechionyia <obi@MacBook-Pro.local>
2022-03-17 20:19:35 +0100

    Create Member model

--- base/models.py
+++ base/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+class Member(models.Model):
+    user = models.ForeignKey(
+    User, on_delete=models.CASCADE, null=True, blank=True)
+    phone = PhoneNumberField(null=False, blank=False, unique=True)
+    created = models.DateTimeField(auto_now_add=True)
