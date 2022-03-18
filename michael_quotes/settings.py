2f5bfb033228aecfff150a71f0f23433e2730cd4
Obi Enechionyia <obienechionyia@gmail.com>
2022-03-18 17:54:15 +0100

    Added send_texts.py, updated .gitignore and settings

--- michael_quotes/settings.py
+++ michael_quotes/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from twilio_keys import DJANGO_SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vxm)#y8bto6u9z!m&bu)xb0^uf0dd9!v*vw-g@@787z9(%@4!2'
+SECRET_KEY = DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
---
+++ send_texts.py
@@ -0,0 +1,48 @@
+import random
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from twilio.rest import Client
+from twilio_keys import TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN
+import requests
+
+account_sid = TWILIO_ACCOUNT_SID
+auth_token = TWILIO_AUTH_TOKEN
+client = Client(account_sid, auth_token)
+
+
+def sendMessage():
+
+    quotes = []
+    url = "https://netflixlife.com/2022/02/18/best-michael-scott-quotes-the-office-every-mood/"
+    driver = webdriver.Chrome("/Users/obi/chromedriver")
+    driver.get(url)
+
+    quoteList = driver.find_elements(By.CSS_SELECTOR,
+                                     "em")
+
+    for quote in quoteList:
+        if quote.text != 'The Office':
+            quotes.append(quote.text)
+
+    driver.quit()
+
+    dailyQuote = "Here's your daily Michael Scott quote! - EasyBot\n\n" + \
+        random.choice(quotes)
+
+    print("Quote found. Sending to recipients...")
+
+    memberDict = {}
+
+    textMessage = requests.get("http://localhost:8000/api/member/").json()
+
+    for i in range(len(textMessage)):
+        memberDict[textMessage[i]['first_name']] = textMessage[i]['number']
+
+    for i in memberDict:
+        client.messages.create(body=dailyQuote,
+                               from_="19106065149",
+                               to=memberDict[i])
+    
+    print("Text messages sent successfully...")
+
+sendMessage()
